87ad4e94a93d43da76328714519d8f80005471aa
ulia141 <uliaputra08@gmail.com>
2021-09-21 00:04:42 +0700

    Initial commit

---
+++ src/main/kotlin/com/uliaid/kotlin/App.kt
@@ -0,0 +1,44 @@
+package com.uliaid.kotlin
+
+/*fun main() {
+    val firstWord = "Dicoding"
+    val lastWord = "Academy"
+    print(firstWord + lastWord)
+}*/
+//variabelvarval
+/*fun main() {
+    val valueA: Int = 10
+    val valueB = 20
+    print(valueA + valueB)
+}*/
+//chart
+/*fun main() {
+    var vocal = 'A'
+
+    println("Vocal " + vocal++)
+    println("Vocal " + vocal++)
+    println("Vocal " + vocal++)
+    println("Vocal " + vocal++)
+    println("Vocal " + vocal--)
+    println("Vocal " + vocal--)
+    println("Vocal " + vocal--)
+    println("Vocal " + vocal--)
+
+}*/
+//string
+/*fun main() {
+    val text  = "Kotlin"
+    val firstChar = text[0]
+
+    print("First character of $text is $firstChar")
+}*/
+/*fun main() {
+    val text  = "Kotlin"
+    for (char in text){
+        print("$char ")
+    }
+}*/
+fun main() {
+    val name = "Unicode test: \u00A9"
+    print(name)
+}
